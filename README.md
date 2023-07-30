4ab1cd5c1253e65fa05bd29ed5febc8644803376
Arif0081 <msazoom22@gmail.com>
2023-07-30 11:40:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fortuknite_flutter
+ A choose your own adventure app which ultimately ends the story of this game
---
+++ android/app/src/main/java/com/example/fortuknite/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.fortuknite;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
